80e182c64d8a5064b8bbee029ad35b006fe0bb7f
paulfersi <128245752+paulfersi@users.noreply.github.com>
2024-06-18 21:24:13 +0200

    Delete build directory

--- build/robbie_simulator/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sonia/robbie-the-robot-arm/V3/src/robbie_simulator
-# Build directory: /home/sonia/robbie-the-robot-arm/build/robbie_simulator
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
