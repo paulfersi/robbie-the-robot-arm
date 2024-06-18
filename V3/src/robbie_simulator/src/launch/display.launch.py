678565c7ca2baf23c341639bf9830f76593d3467
paulfersi <paolfersi@gmail.com>
2024-06-18 17:42:21 +0200

    modified launcher

--- V3/src/robbie_simulator/src/launch/display.launch.py
+++ V3/src/robbie_simulator/src/launch/display.launch.py
@@ -16,7 +16,6 @@ def generate_launch_description():
 
     urdf_model = LaunchConfiguration('urdf_model')
     rviz_config_file = LaunchConfiguration('rviz_config_file')
-    use_robot_state_pub = LaunchConfiguration('use_robot_state_pub')
 
     # Declare the launch arguments  
     declare_urdf_model_path_cmd = DeclareLaunchArgument(
@@ -28,6 +27,24 @@ def generate_launch_description():
     name='rviz_config_file',
     default_value=rviz_config_path,
     description='Full path to the RVIZ config')
+
+    start_joint_state_publisher_cmd = Node(
+    package='joint_state_publisher',
+    executable='joint_state_publisher',
+    name='joint_state_publisher')
+
+    start_joint_state_publisher_gui_node = Node(
+    package='joint_state_publisher_gui',
+    executable='joint_state_publisher_gui',
+    name='joint_state_publisher_gui')
+ 
+  # Subscribe to the joint states of the robot, and publish the 3D pose of each link.
+    start_robot_state_publisher_cmd = Node(
+    package='robot_state_publisher',
+    executable='robot_state_publisher',
+    parameters=[{'use_sim_time': True, 
+    'robot_description': Command(['cat ', urdf_model])}],
+    arguments=[urdf_model_path])
 
     start_robot_state_publisher_cmd = Node(
     package='robot_state_publisher',
@@ -47,6 +64,8 @@ def generate_launch_description():
 
     ld.add_action(declare_urdf_model_path_cmd)
     ld.add_action(declare_rviz_config_file_cmd)
+    ld.add_action(start_joint_state_publisher_cmd)
+    ld.add_action(start_joint_state_publisher_gui_node)
 
     #ld.add_action(start_robot_state_publisher_cmd)
     ld.add_action(start_rviz_cmd)
