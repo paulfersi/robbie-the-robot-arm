46a7eee40d0020a95d9ba381de48d37bda8f569b
paulfersi <128245752+paulfersi@users.noreply.github.com>
2024-06-18 21:21:12 +0200

    changes

--- install/robbie_simulator/share/robbie_simulator/launch/display.launch.py
+++ install/robbie_simulator/share/robbie_simulator/launch/display.launch.py
@@ -12,11 +12,10 @@ def generate_launch_description():
     rviz_config_path = os.path.join(pkg_share, '/src/rviz/robbie.rviz')
  
     #path to the URDF file
-    urdf_model_path = os.path.join(pkg_share, '/src/urdf/robbie.urdf')
+    urdf_model_path = os.path.join(pkg_share, '/src/urdf/test_robot.xacro')
 
     urdf_model = LaunchConfiguration('urdf_model')
     rviz_config_file = LaunchConfiguration('rviz_config_file')
-    use_robot_state_pub = LaunchConfiguration('use_robot_state_pub')
 
     # Declare the launch arguments  
     declare_urdf_model_path_cmd = DeclareLaunchArgument(
@@ -29,12 +28,30 @@ def generate_launch_description():
     default_value=rviz_config_path,
     description='Full path to the RVIZ config')
 
-    #start_robot_state_publisher_cmd = Node(
-    #package='robot_state_publisher',
-    #executable='robot_state_publisher',
-    #parameters=[{'use_sim_time': True, 
-    #'robot_description': Command(['xacro ', urdf_model])}],
-    #arguments=[urdf_model_path])
+    start_joint_state_publisher_cmd = Node(
+    package='joint_state_publisher',
+    executable='joint_state_publisher',
+    name='joint_state_publisher')
+
+    start_joint_state_publisher_gui_node = Node(
+    package='joint_state_publisher_gui',
+    executable='joint_state_publisher_gui',
+    name='joint_state_publisher_gui')
+ 
+  # Subscribe to the joint states of the robot, and publish the 3D pose of each link.
+    start_robot_state_publisher_cmd = Node(
+    package='robot_state_publisher',
+    executable='robot_state_publisher',
+    parameters=[{'use_sim_time': True, 
+    'robot_description': Command(['xacro ', urdf_model])}],
+    arguments=[urdf_model_path])
+
+    start_robot_state_publisher_cmd = Node(
+    package='robot_state_publisher',
+    executable='robot_state_publisher',
+    parameters=[{'use_sim_time': True, 
+    'robot_description': Command(['xacro ', urdf_model])}],
+    arguments=[urdf_model_path])
 
     start_rviz_cmd = Node(
     package='rviz2',
@@ -47,6 +64,8 @@ def generate_launch_description():
 
     ld.add_action(declare_urdf_model_path_cmd)
     ld.add_action(declare_rviz_config_file_cmd)
+    ld.add_action(start_joint_state_publisher_cmd)
+    ld.add_action(start_joint_state_publisher_gui_node)
 
     #ld.add_action(start_robot_state_publisher_cmd)
     ld.add_action(start_rviz_cmd)
