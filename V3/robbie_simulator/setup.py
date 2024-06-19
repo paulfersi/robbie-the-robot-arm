a5665c71a175a6daa62b4d0e73d3370cecf60825
paulfersi <128245752+paulfersi@users.noreply.github.com>
2024-06-19 09:42:51 +0200

    bugs solved

--- V3/robbie_simulator/setup.py
+++ V3/robbie_simulator/setup.py
@@ -10,6 +10,10 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/urdf', ['urdf/robbie.urdf']),
+        ('share/' + package_name + '/launch', ['launch/joint_state_publisher_launch.py']),
+        ('share/' + package_name + '/launch', ['launch/robot_description_launch.py']),
+        
     ],
     install_requires=['setuptools'],
     zip_safe=True,
