ac84e3b194d2444490363030ad9ec52f4cf50d6a
paulfersi <128245752+paulfersi@users.noreply.github.com>
2024-06-19 09:21:19 +0200

    ament_python instead of cmake

---
+++ V3/robbie_simulator/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'robbie_simulator'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='sonia',
+    maintainer_email='128245752+paulfersi@users.noreply.github.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'joint_state_publisher_gui = robbie_simulator.joint_state_publisher_gui:main',
+        ],
+    },
+)
